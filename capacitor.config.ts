6dc5f1f58e898750fd7bff7d1b812e000105a94d
cmcampione <cmcampione@gmail.com>
2025-03-02 08:30:02 +0100

    Adding Capacitor support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.thingshub.bitsofmylife',
+  appName: 'bits-of-my-life',
+  webDir: 'dist/bits-of-my-life/browser',
+};
+
+export default config;
